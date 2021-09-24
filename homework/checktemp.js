66a5798e93e90c7976c4f80266f0f7828a359691
Taaniel-Ztan <90251748+Taaniel-Ztan@users.noreply.github.com>
2021-09-24 12:14:13 +0300

    Update checktemp.js

--- homework/checktemp.js
+++ homework/checktemp.js
@@ -1,1 +1,34 @@
+///CHECK TEMP KODUTÖÖ 1///
 
+//meetod1///
+
+
+function checkTemp (temp) {
+    // let temp = 10;
+
+    if (temp < 21){
+        return("Temperatuur on liiga külm")
+    }
+    if (temp >= 21 && temp <= 40){
+        return("Temperatuur on soe")
+    }
+    if (temp > 40){
+        return("Temperatuur on kuum")
+    }
+}
+
+///meetod2///
+
+function checkTemp (temp) {
+//  let temp = 5;
+
+if (temp < 21) {
+    return -1
+    } 
+if(temp >= 21 && temp <= 40) {
+    return 0
+    } 
+if (temp > 41) {
+    return 1
+    }
+}
